b4b7305e357c29394706605a694eca16167a54a2
Jad Saad <jadsaad06@gmail.com>
2024-12-01 21:46:16 -0800

    Set up all user controls in main

--- Main.py
+++ Main.py
@@ -39,17 +39,51 @@ def main():
     while choice != 5:
         displayMenu()
         try:
-            choice = int(input("\nEnter your choice: user"))
+            choice = int(input("\nEnter your choice: "))
+            
             if choice == 1:
                 channelName = joinChannel(server, member)
-                print("Joining a channel...")
+
+
+                
+                while channelName is not None:
+                    displayChannelMenu()
+                    channelChoice = 0
+
+                    # Validate Input
+                    while not valid:
+                        try:
+                            choice = int(input("\nEnter here: "))
+                            
+                            if choice in [1, 2, 3]:
+                                valid = True
+                            else:
+                                displayChannelMenu()
+                        except ValueError:
+                            displayChannelMenu()
+                    
+                    if choice == 1:
+                        message = input("Input Message: ")
+
+                        server.postMessage(member, channelName, message)
+                    else:
+                        server.displayChannelMembers()
+
+                            
+
+
             elif choice == 2:
-                print("Viewing messages...")
+                # Show current interactions
+                    server.displayChatLog(channelName)
+
             elif choice == 3:
-                print("Sending a message...")
+                    message = input("Input Message: ")
+                    server.postMessage(member, channelName, message)
+
             elif choice == 4:
                 if member.hasPermission("create_channel"):
-                    print("Admin actions...")
+                    displayAdminMenu()
+
                 else:
                     print("Only Admins can perform this action.")
             elif choice == 5:
@@ -63,9 +97,22 @@ def main():
 def displayMenu() -> None:
     print("""\n1. Join a Channel: Users can select a channel to join.
 2. View Messages: Users can see the most recent messages posted in the channel.
-3. Send a Message: Users can post a message or send a private message.
+3. Send a Message: Users can post a message or send a private message. (private messaging will be added in prog5)
 4. Admin Actions (if applicable): Administrators can add or remove users, and delete inappropriate messages.
 5. Exit System: Ends the session.""")
+
+def displayChannelMenu():
+    print("""\n1. Post a Message
+2. List All Users in Current Channel
+3. Go Back""")
+
+def displayAdminMenu():
+    print("""Admin Console: 
+1. Add User 
+2. Remove User 
+3. Delete Message.""")
+
+
     
 def joinChannel(server: Server, member: Member) -> str:
     success = False
